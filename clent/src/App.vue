3de22948657edf88609407e575aac67e3efa10e3
vahan <vahan03031996@gmail.com>
2021-04-02 22:06:56 +0400

    axios

---
+++ clent/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div v-for="product in products">
+      <p>name: {{product.name}}</p>
+      <p>name: {{product.price}}</p>
+      <p>name: {{product.count}}</p>
+      <p>name: {{product.views}}</p>
+      <p>name: {{product.description}}</p>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.getProducts()
+  },
+  methods: {
+    getProducts(){
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ clent/src/components/User.vue
@@ -0,0 +1,13 @@
+<template>
+<div>etheth</div>
+</template>
+
+<script>
+export default {
+name: "User"
+}
+</script>
+
+<style scoped>
+
+</style>
